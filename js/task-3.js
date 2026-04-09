bc43c998f9c1222a65b36aac076f90341ab7e3c4
nick3fc <nick3fc@gmail.com>
2026-04-09 22:20:54 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,9 @@
+function getElementWidth(content, padding, border) {
+  let ElementWidth =
+    parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
+  return ElementWidth;
+}
+
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
